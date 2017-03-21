f8f1c42252935cc671487c84fe53e65431c14a45
Trevor Preston <trevorpreston4@gmail.com>
2017-03-21 12:30:52 -0400

    add SavedPet model

---
+++ server/app/models/saved_pet.rb
@@ -0,0 +1,2 @@
+class SavedPet < ApplicationRecord
+end
---
+++ server/test/models/saved_pet_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class SavedPetTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
