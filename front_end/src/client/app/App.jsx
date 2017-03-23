b98093ce81c20d81feed0d626103a3cac1efa1f9
Trevor Preston <trevorpreston4@gmail.com>
2017-03-23 16:59:14 -0400

    add searchbar component and placeholder

--- front_end/src/client/app/App.jsx
+++ front_end/src/client/app/App.jsx
@@ -1,6 +1,7 @@
 import React            from 'react'
 import ReactDOM         from 'react-dom'
 import Savedpets 				from './Savedpets.jsx'
+import SearchPets				from './SearchPets.jsx'
 
 
 export default class App extends React.Component{
@@ -27,6 +28,7 @@ export default class App extends React.Component{
 	render(){
 		return (
 			<div>
+				<SearchPets />
 				<button onClick={() => this.getSavedPets()}> Get dem pets </button>
 				<Savedpets petsFromState = {this.state.savedPets}/>
 			</div>
---
+++ front_end/src/client/app/SearchPets.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class SearchPets extends React.Component {
+	render(){
+		return (
+			<div>
+				<h1> this is a future search bar </h1>
+			</div>
+		)
+	}
+}
