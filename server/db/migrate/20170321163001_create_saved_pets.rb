d78b89d1fdf6822e17fee405e8ba44ac3da08bc0
Trevor Preston <trevorpreston4@gmail.com>
2017-03-23 10:09:19 -0400

    add seed file, fixed createdb migration

--- server/db/migrate/20170321163001_create_saved_pets.rb
+++ server/db/migrate/20170321163001_create_saved_pets.rb
@@ -1,10 +1,10 @@
 class CreateSavedPets < ActiveRecord::Migration[5.0]
   def change
     create_table :saved_pets do |t|
-      t.string, :animal_breed
-      t.string, :animal_name
-      t.integer, :animal_id
-      t.string, :city
+      t.string :animal_breed
+      t.string :animal_name
+      t.integer :animal_id
+      t.string :city
       t.integer :zip
 
       t.timestamps
--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+pets = [
+	{animal_breed: "Doberman", animal_name: "Matt", animal_id: "A528175", city: "Kenmore", zip: 98267},
+	{animal_breed: "Husky", animal_name: "Lester", animal_id: "A529173", city: "Seattle", zip: 98126},
+	{animal_breed: "Mut", animal_name: "Spike", animal_id: "A527549", city: "Shoreline", zip: 98542},
+	{animal_breed: "Golden Retreiver", animal_name: "Table", animal_id: "A579201", city: "Mukilteo", zip: 97642},
+	{animal_breed: "Laberdoodle", animal_name: "Frank", animal_id: "A521382", city: "Pandora", zip: 98125},
+	{animal_breed: "Fox", animal_name: "Stu", animal_id: "A524098", city: "Bellevue", zip: 98652},
+	{animal_breed: "Beagle", animal_name: "Spot", animal_id: "A526732", city: "LFP", zip: 98611},
+]
+
+
+SavedPet.create(pets)
